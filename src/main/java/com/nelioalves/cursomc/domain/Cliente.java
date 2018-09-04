e1261572ace3e27ecf207a371125e76b6f8546aa
Henrique Cavalcante <=>
2018-09-04 19:56:22 -0300

    PUT, DELETE e GET para Cliente

--- src/main/java/com/nelioalves/cursomc/domain/Cliente.java
+++ src/main/java/com/nelioalves/cursomc/domain/Cliente.java
@@ -49,7 +49,7 @@ public class Cliente implements Serializable{
 		this.nome = nome;
 		this.email = email;
 		this.cpfOuCnpj= cpfOuCnpj;
-		this.tipo = tipo.getCod(); // IMPORTANTE
+		this.tipo = (tipo == null) ? null : tipo.getCod(); // IMPORTANTE
 	}
 
 	public Integer getId() {
